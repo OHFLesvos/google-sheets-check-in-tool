0aa516579e98037b8fda7c60051b2de5a09ea5b5
Nicolas Perrenoud <mail@nicu.ch>
2021-04-18 22:57:00 +0300

    refactor

--- src/oauth.js
+++ src/oauth.js
@@ -1,13 +1,9 @@
 import { OAuth2Client } from "google-auth-library";
 
-const oAuth2Client = new OAuth2Client(
-  process.env.VUE_APP_GOOGLE_OAUTH_CLIENT_ID,
-  process.env.VUE_APP_GOOGLE_OAUTH_CLIENT_SECRET,
-  process.env.VUE_APP_GOOGLE_REDIRECT_URL
-);
+const oAuth2Client = new OAuth2Client({
+    clientId: process.env.VUE_APP_GOOGLE_OAUTH_CLIENT_ID,
+    clientSecret: process.env.VUE_APP_GOOGLE_OAUTH_CLIENT_SECRET,
+    redirectUri: process.env.VUE_APP_GOOGLE_REDIRECT_URL
+});
 
 export default oAuth2Client;
-// const oauthClient = new OAuth2Client({
-//     clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET
-//   });

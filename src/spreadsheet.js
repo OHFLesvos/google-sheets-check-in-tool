b44660fc94aaa8e9e1eee9f039a6a1473d1073ad
Nicolas Perrenoud <mail@nicu.ch>
2021-04-19 00:09:00 +0300

    cleanup

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -5,10 +5,6 @@ export default {
   async fetchSpreadsheetDocument() {
     const doc = new GoogleSpreadsheet(process.env.VUE_APP_SPREADSHEET_ID);
     doc.useOAuth2Client(oauth.getClient());
-    // await doc.useServiceAccountAuth({
-    //   client_email: process.env.VUE_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    //   private_key: process.env.VUE_APP_GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-    // });
     await doc.loadInfo();
     return doc;
   },

a59a88c3286b3cad391f7127b4f7b2757a4cac2d
Nicolas Perrenoud <mail@nicu.ch>
2021-04-18 15:31:08 +0300

    Brand link to root

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">{{ title }}</a>
+      <a class="navbar-brand" href="/">{{ title }}</a>
       <ul class="navbar-nav ms-auto">
         <li v-if="externalUrl" class="nav-item">
           <a :href="externalUrl" target="_blank" class="nav-link"

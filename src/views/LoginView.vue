67555b7e6a36757a9c6f90d2f91364d40c0281d7
Nicolas Perrenoud <nicolas@ohf-lesvos.org>
2021-04-20 12:30:38 +0300

    Login restyle

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="d-grid">
-    <button type="button" class="btn btn-primary" @click="signIn">
-      <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google' }" /> Login
-      with Google
-    </button>
+  <div class="card mx-auto" style="max-width: 25rem">
+    <div class="card-body">
+      <h5 class="card-title">Login required</h5>
+      <p class="card-text">You need to login in order to proceed.</p>
+      <div class="d-grid">
+      <button type="button" class="btn btn-primary" @click="signIn">
+        <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google' }" />
+        Login with Google
+      </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,13 +17,10 @@
 import oauth from "../oauth";
 
 export default {
-  async mounted() {
-    this.$store.commit("setTitle", "Login");
-  },
   methods: {
     signIn() {
       oauth.signIn();
-    }
-  }
+    },
+  },
 };
 </script>
